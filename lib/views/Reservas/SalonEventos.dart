9cb41089fd9850a7dbb476c6381e338ba5c45cff
Dussan01 <Dussan01@users.noreply.github.com>
2020-06-02 15:50:15 -0500

    Reserva

--- lib/views/Reservas/SalonEventos.dart
+++ lib/views/Reservas/SalonEventos.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:disenoapp/views/ReclamosPage.dart';
 import 'package:flutter/material.dart';
 
 class SalonEventosPage extends StatefulWidget {
@@ -11,7 +12,8 @@ class _SalonEventosPageState extends State<SalonEventosPage> {
 
   String nombre;
   String fecha;
-  TimeOfDay hora;
+  String hora;
+
   bool _validar() {
     final form = _formKey.currentState;
     if (form.validate()) {
@@ -42,92 +44,21 @@ class _SalonEventosPageState extends State<SalonEventosPage> {
   DateTime _dateTime;
   @override
   Widget build(BuildContext context) {
+    CommonThings.size = MediaQuery.of(context).size;
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Reserva'),
-          backgroundColor: Colors.yellow,
-        ),
-        body: ListView(
-          children: <Widget>[
-            Form(
-              key: _formKey,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: <Widget>[
-                    // TextFormField(decoration: InputDecoration(labelText: '')),
-                    TextFormField(
-                      decoration: InputDecoration(labelText: 'Nombre'),
-                      validator: (value) {
-                        if (value.isEmpty) {
-                          return 'Por favor ingrese un nombre';
-                        }
-                      },
-                      onSaved: (value) => nombre = value.trim(),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    // TextFormField(
-                    //   keyboardType: TextInputType.phone,
-                    //   decoration: InputDecoration(labelText: 'Hora'),
-                    //   validator: (value) {
-                    //     if (value.isEmpty) {
-                    //       return 'Por favor ingrese un hora';
-                    //     }
-                    //   },
-                    //   onSaved: (value) => hora = value.trim(),
-                    // ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(_dateTime == null
-                        ? 'No has seleccionado'
-                        : _dateTime.toString()),
-                    Text(hora == null
-                        ? 'No has seleccionado'
-                        : _dateTime.toString()),
-                    RaisedButton(
-                      child: Text('Selecciona fecha'),
-                      onPressed: () {
-                        showDatePicker(
-                                context: context,
-                                initialDate: DateTime.now(),
-                                firstDate: DateTime(2001),
-                                lastDate: DateTime(2222))
-                            .then((date) {
-                            
-                          setState(() {
-                            _dateTime = date;
-                          });
-                        });
-                      },
-                    ),
-                    RaisedButton(
-                      child: Text('Selecciona Hora'),
-                      onPressed: () {
-                        showTimePicker(
-                                context: context, initialTime: TimeOfDay.now())
-                            .then((time) {
-                          setState(() {
-                            hora = time;
-                          });
-                        });
-                      },
-                    ),
-                    RaisedButton(
-                      onPressed: _enviar,
-                      child: Text("Guardar"),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            StreamBuilder(
+      appBar: AppBar(
+        title: Text('Reserva'),
+        backgroundColor: Colors.yellow,
+      ),
+      body: Stack(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 280),
+            child: StreamBuilder(
               stream: Firestore.instance
                   .collection('reserva')
                   // .document(widget.id)
-                  // .collection('infoComida')
+                  // .collection('mycolrecipes')
                   .snapshots(),
               builder: (BuildContext context,
                   AsyncSnapshot<QuerySnapshot> snapshot) {
@@ -148,7 +79,7 @@ class _SalonEventosPageState extends State<SalonEventosPage> {
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: <Widget>[
                                 Text(
-                                  '\n Esperando Pedidos.\n',
+                                  '\nAdd my recipe.\n',
                                   style: TextStyle(
                                       fontSize: 24, color: Colors.blue),
                                 )
@@ -159,6 +90,9 @@ class _SalonEventosPageState extends State<SalonEventosPage> {
                       ),
                     );
                   } else {
+                    //print("from the streamBuilder: "+ snapshot.data.documents[]);
+                    // print(length.toString() + " doc length");
+
                     return ListView(
                       children: snapshot.data.documents.map((document) {
                         return Card(
@@ -176,80 +110,28 @@ class _SalonEventosPageState extends State<SalonEventosPage> {
                                       fontSize: 17.0,
                                     ),
                                   ),
-
-                                  subtitle: Text(
-                                    'hora ' +
-                                        document['hora']
-                                            .toString()
-                                            .toUpperCase(),
-                                    style: TextStyle(
-                                        color: Colors.black, fontSize: 12.0),
+                                  subtitle: Row(
+                                    children: <Widget>[
+                                      Text(
+                                        'Fecha: ' +
+                                        document['fecha'].toDate().toString(),
+                                        style: TextStyle(
+                                            color: Colors.black, fontSize: 12.0),
+                                            maxLines: 1,
+                                      ),
+                                      Text(
+                                        ' Hora: ' + 
+                                        document['hora'].toString(),
+                                        style: TextStyle(
+                                            color: Colors.black, fontSize: 12.0),
+                                            maxLines: 1,
+                                      ),
+                                    ],
                                   ),
-                                  //editar la receta
-                                  onTap: () {
-                                    // Producto infoComida = Producto(
-                                    //   uid: document['uid'],
-                                    //   titulo: document['titulo'],
-                                    //   imagen: document['image'],
-                                    //   descripcion: document['descripcion'],
-                                    //   precio: document['precio'],
-                                    //   categoria: document['categoria']
 
-                                    // );
-                                    // Navigator.push(
-                                    //     context,
-                                    //     MaterialPageRoute(
-                                    //         builder: (context) => EditInfo(
-                                    //             info: infoComida,
-                                    //             idInfo: document.documentID,
-                                    //             uid: userID)));
-                                  },
+                                  //editar la receta
+                                  onTap: () {},
                                 ),
-                              ),
-                              IconButton(
-                                icon: Icon(
-                                  Icons.delete,
-                                  color: Colors.redAccent,
-                                ),
-                                onPressed: () {
-                                  //eliminamos la receta personal
-                                  // document.data.remove('key');
-                                  // Firestore.instance
-                                  //     .collection('infoComida')
-                                  //     .document(document.documentID)
-                                  //     .delete();
-                                  // //eliminamos la foto
-                                  // FirebaseStorage.instance
-                                  //     .ref()
-                                  //     .child(
-                                  //         'infoComida/$userID/uid/categoria/${document['titulo'].toString()}.jpg')
-                                  //     .delete()
-                                  //     .then((onValue) {
-                                  //   print('foto eliminada');
-                                  // });
-                                }, //funciona
-                              ),
-                              IconButton(
-                                icon: Icon(
-                                  Icons.remove_red_eye,
-                                  color: Colors.blueAccent,
-                                ),
-                                //Visualizar la receta,
-                                onPressed: () {
-                                  // Info infoDescripcion = Info(
-                                  //   titulo: document['titulo'].toString(),
-                                  //   descripcion: document['descripcion'].toString(),
-                                  //   paso: document['paso'].toString(),
-                                  //   image: document['image']
-                                  // );
-                                  // Navigator.push(
-                                  //     context,
-                                  //     MaterialPageRoute(
-                                  //         builder: (context) => VerInfo(
-                                  //             infoDecripcion: infoDescripcion,
-                                  //             idInfo: document.documentID,
-                                  //             uid: userID)));
-                                },
                               ),
                             ],
                           ),
@@ -260,7 +142,76 @@ class _SalonEventosPageState extends State<SalonEventosPage> {
                 }
               },
             ),
-          ],
-        ));
+          ),
+          ListView(
+            children: <Widget>[
+              Form(
+                key: _formKey,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: <Widget>[
+                      // TextFormField(decoration: InputDecoration(labelText: '')),
+                      TextFormField(
+                        decoration: InputDecoration(labelText: 'Nombre'),
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Por favor ingrese un nombre';
+                          }
+                        },
+                        onSaved: (value) => nombre = value.trim(),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(labelText: 'Hora'),
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Por favor ingrese un hora';
+                          }
+                        },
+                        onSaved: (value) => hora = value.trim(),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(_dateTime == null
+                          ? 'No has seleccionado'
+                          : _dateTime.toString()),
+
+                      RaisedButton(
+                        child: Text('Selecciona fecha'),
+                        onPressed: () {
+                          showDatePicker(
+                                  context: context,
+                                  initialDate: DateTime.now(),
+                                  firstDate: DateTime(2001),
+                                  lastDate: DateTime(2222))
+                              .then((date) {
+                            setState(() {
+                              _dateTime = date;
+                            });
+                          });
+                        },
+                      ),
+
+                      RaisedButton(
+                        onPressed: _enviar,
+                        child: Text("Guardar"),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
